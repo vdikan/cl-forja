9eb56d466f2de194bfca09da3235f164332fbd94
Vladimir Dikan <vdikan@vivaldi.net>
2020-03-09 18:04:09 +0100

    draft of mk-calculation
    Error occurs on external call to body-form

--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,8 @@
 (defpackage cl-forja
-  (:use :cl)
-  (:export :unroll-plist-f))
+  (:use :cl :lisp-namespace)
+  (:export :unroll-plist-f
+           :mk-calculation))
+
 (in-package :cl-forja)
 
 
@@ -34,3 +36,55 @@ where FUNC is applied to superseeding list member upon creation of plist."
                   (setf recur
                         (append recur (apply #'unroll-plist-f r (cdr key-conses)))))
                 recur)))))
+
+
+(lol:defmacro! mk-calculation (plist &body body)
+  `(let ((,g!params ,plist)            ; nslet? for/from lisp-namespace
+         (,g!runner-form '(,@body))    ; TODO: print to string, no quoting
+         (,g!status 'new))
+     (labels ((all-params () ,g!params)
+              (get-param (key) (getf ,g!params key))
+              (set-param (key val) (setf (getf ,g!params key) val))
+              (get-status () ,g!status)
+              (set-status (sym) (setf ,g!status sym)))
+       (lol:dlambda
+        (:get (key) (get-param key))
+        (:status () ,g!status)
+        (:show-runner () ,g!runner-form)
+        (:run () ,@body (set-status 'finished))
+        (t () (cons (cons 'calculation ,g!status) (list ,g!params)))))))
+
+
+
+;; ;;;;;;;test area;;;;;;;;
+;; (defparameter props
+;;   '(:label "si8"
+;;     :alat  (10.16 10.26 10.36 10.46)
+;;     :alat-units "Bohr"
+;;     :code ("Siesta" "QE")
+;;     :bands ("G" "X" "L" "K" "G")))
+
+;; (print
+;;  (macroexpand '(mk-calculation props
+;;                (set-param :very-new '("Works?" "Yes!"))
+;;                (set-param :rather-old '("Should work?" "Also true"))) ))
+
+
+;; ;;;;;;;;;;;;;;;;;;;;
+;; (define-namespace calcs)
+
+;; (setf (symbol-calcs 'calctest)
+;;       (mk-calculation props
+;;         (set-param :very-new '("Works?" "Yes!"))
+;;         (set-param :rather-old '("Should work?" "Also true"))))
+
+;; (print
+;;  (funcall (symbol-calcs 'calctest) :show-runner))
+
+;; *calcs-table*
+
+;; (loop
+;;    for key being the hash-keys of *calcs-table*
+;;    ;; using (hash-value value)
+;;    do (print (funcall (symbol-calcs key) :show-runner)))
+;;    ;; do (print (funcall value :show-runner)))
--- tests/main.lisp
+++ tests/main.lisp
@@ -6,26 +6,43 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-forja)' in your Lisp.
 
+
 (deftest test-unroll-plist-f
   (testing "should unroll plists okay"
-    (let* ((props '(:label "si8"
+    (let* ((props '(:label "si"
                     :alat  (10.16 10.26 10.36 10.46)
                     :alat-units "Bohr"
-                    :code ("Siesta" "QE")
                     :bands ("G" "X" "L" "K" "G")))
            (unrolled
             (cl-forja:unroll-plist-f
-             props :alat :code
-             (list :bands #'(lambda (s) (format nil "~a-point" s))))))
-      (ok (member '(:LABEL "si8"
+             props :alat (list :bands
+                               #'(lambda (s)
+                                   (format nil "~a-point" s))))))
+      (ok (member '(:LABEL "si"
                     :ALAT 10.16
                     :ALAT-UNITS "Bohr"
-                    :CODE "Siesta"
                     :BANDS "L-point")
                   unrolled :test 'equal))
-      (ok (member '(:LABEL "si8"
+      (ok (member '(:LABEL "si"
                     :ALAT 10.46
                     :ALAT-UNITS "Bohr"
-                    :CODE "QE"
                     :BANDS "G-point")
                   unrolled :test 'equal)))))
+
+
+(deftest test-calculation-closure
+  (testing "`mk-calculation` should return a dlambda-closure
+ around loggable-style plist"
+    (let* ((props '(:label "si"
+                    :alat  10.26
+                    :alat-units "Bohr"))
+           (calc (cl-forja:mk-calculation props
+                   (set-param :title "Si structure DFT calc")
+                   (set-param :code  '("Siesta" "QE")))))
+      ;; (setf (symbol-function 'calc)
+      ;;       (cl-forja:mk-calculation props
+      ;;         (set-param :title "Si structure DFT calc")
+      ;;         (set-param :code  '("Siesta" "QE"))))
+      (ok (= (funcall (symbol-function 'calc) :get :alat) 10.26))
+      (ok (eq (funcall (symbol-function 'calc) :run) 'finished))
+      )))
