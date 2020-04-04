ac4a6134afd7119c87d8db43064a377a65de1f82
Vladimir Dikan <vdikan@vivaldi.net>
2020-04-04 20:38:11 +0200

    Init draft of QE template plugin

---
+++ plugins/qe.lisp
@@ -0,0 +1,107 @@
+(defpackage cl-forja/qe
+  (:use :cl
+        :cl-arrows
+        :cl-forja/templates
+        :cl-forja/cstructs
+        :cl-forja/lattices)
+  (:import-from #:serapeum
+                #:assocdr)
+  (:export #:number-of-atoms-to-template
+           #:number-of-kinds-to-template
+           #:atom-list-to-template
+           #:kinds-to-template
+           #:lattice-to-template
+           #:cstruct-to-template))
+
+(in-package :cl-forja/qe)
+
+
+(defmethod number-of-atoms-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (plist-to-template
+   (list :number-of-atoms (number-of-atoms cs)) tplt))
+
+
+(defmethod number-of-kinds-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (plist-to-template
+   (list :number-of-kinds (number-of-kinds cs)) tplt))
+
+
+(defun qe-atom-list (cs)
+  (let ((res ""))
+    (dolist (l (cstruct-atoms cs) res)
+      (setf res
+            (format nil "~a~&~a" res
+                    (format nil"~{~&~{~4a ~,12@f ~,12@f ~,12@f~}~}"
+                            (map 'list (lambda (vec) (list
+                                                      (string-capitalize
+                                                       (symbol-name (car l)))
+                                                      (aref vec 0)
+                                                      (aref vec 1)
+                                                      (aref vec 2)))
+                                 (cdr l))))))))
+
+
+(defmethod atom-list-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (plist-to-template
+   (list :atom-list (qe-atom-list cs)) tplt))
+
+
+(defun qe-kinds (cs)
+  (let ((res ""))
+    (dolist (l (cstruct-atoms cs) res)
+      (let ((kind-sym (car l)))
+        (setf res
+              (format nil "~a~&~a" res
+                      (format nil "~{~&~4a ~f ~16@a~}"
+                              (list
+                               (string-capitalize (symbol-name kind-sym))
+                               (chem-kind-mass (assocdr kind-sym (cstruct-kinds cs)))
+                               (chem-kind-qe-pseudo (assocdr kind-sym (cstruct-kinds cs)))))))))))
+
+
+(defmethod kinds-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (plist-to-template
+   (list :chem-kinds (qe-kinds cs)) tplt))
+
+
+;;; On-lattice-dispatch section.
+;; Well, have a look at this:
+;; https://www.quantum-espresso.org/Doc/INPUT_PW.html#ibrav
+
+(declaim (ftype (function (lattice) real) celldm1))
+
+(defun celldm1 (latt)
+  "Get the celldm1 for the LATTice in corect units, that is `Bohr`."
+  (let ((alat (lattice-alat latt)))
+    (if (string-equal (lattice-units latt) "Ang")
+        (* alat 1.8897259885)           ; convert Ang to Bohr
+        alat)))
+
+
+(defgeneric qe-lattice-properties (latt))
+
+(defmethod qe-lattice-properties ((latt lattice-Cubic))
+  (list :ibrav "1" :celldm1 (format nil "~,8f" (celldm1 latt))))
+
+(defmethod qe-lattice-properties ((latt lattice-cF))
+  (list :ibrav "2" :celldm1 (format nil "~,8f" (celldm1 latt))))
+
+
+(defmethod lattice-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (plist-to-template
+   (qe-lattice-properties (cstruct-lattice cs)) tplt))
+
+
+(defmethod cstruct-to-template
+    ((tplt string) (cs cstruct) (code-spec (eql :qe)))
+  (-> tplt
+      (number-of-atoms-to-template cs :qe)
+      (number-of-kinds-to-template cs :qe)
+      (kinds-to-template cs :qe)
+      (lattice-to-template cs :qe)
+      (atom-list-to-template cs :qe)))
