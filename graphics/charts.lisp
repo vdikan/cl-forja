3c91b162c96b7cd1d5d1b622c4aa05d654963718
Vladimir Dikan <vdikan@vivaldi.net>
2020-06-03 22:07:36 +0200

    lambda-accessors can be passed to quick-chart-data

--- graphics/charts.lisp
+++ graphics/charts.lisp
@@ -13,40 +13,38 @@
 (in-package :cl-forja/charts)
 
 
-(defun quick-chart-data (plists-col x-kw kwpairs)
+(defun quick-chart-data (plists-col x-kw y-data-specs)
   (let* ((xdat (mapcar (lambda (l) (getf l x-kw)) plists-col))
          (data nil))
-    (loop for kwp in kwpairs
+    (loop for kwp in y-data-specs
           do (progn
                (push xdat data)
                (cond
-                ((keywordp kwp)
-                 (progn
-                   (push (mapcar (lambda (l) (getf l kwp))
-                                 plists-col) data)
-                   (push (symbol-name kwp) data)))
-                ((listp kwp)
-                 (progn
-                   (push (mapcar (lambda (l) (getf l (car kwp)))
-                                 plists-col) data)
-                   (push (cadr kwp) data))))))
+                 ((keywordp kwp)
+                  (progn
+                    (push (mapcar (lambda (l) (getf l kwp)) plists-col) data)
+                    (push (symbol-name kwp) data)))
+                 ((listp kwp)
+                  (destructuring-bind (kw desc-str &optional (func #'identity)) kwp
+                    (push (mapcar (lambda (l) (funcall func (getf l kw))) plists-col) data)
+                    (push desc-str data))))))
     (reverse data)))
 
 
-(defun quick-chart-plist (plists-col x-kw kwpairs
+(defun quick-chart-plist (plists-col x-kw y-data-specs
                           &key xlabel ylabel title)
   (vgplot:title (or title ""))
   (vgplot:xlabel (or xlabel ""))
   (vgplot:ylabel (or ylabel ""))
-  (apply #'vgplot:plot (quick-chart-data plists-col x-kw kwpairs)))
+  (apply #'vgplot:plot (quick-chart-data plists-col x-kw y-data-specs)))
 
 
-(defun quick-chart (session-lst x-kw kwpairs &key xlabel ylabel title)
-  (let* ((kws-lst (loop for kwp in (append (list x-kw) kwpairs)
+(defun quick-chart (session-lst x-kw y-data-specs &key xlabel ylabel title)
+  (let* ((kws-lst (loop for kwp in (append (list x-kw) y-data-specs)
                         collect (if (keywordp kwp) kwp (car kwp))))
          (plist-col (loop for calc in session-lst
                           collect (apply #'get-params-from calc kws-lst))))
     (vgplot:title (or title ""))
     (vgplot:xlabel (or xlabel ""))
     (vgplot:ylabel (or ylabel ""))
-    (apply #'vgplot:plot (quick-chart-data plist-col x-kw kwpairs))))
+    (apply #'vgplot:plot (quick-chart-data plist-col x-kw y-data-specs))))
